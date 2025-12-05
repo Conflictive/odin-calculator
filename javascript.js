7ee82756195b156affc10a51132cd799bbb48d11
Conflictive <callumtolland@gmail.com>
2025-12-05 16:36:17 +0000

    Implement operator mapping for calculator operations

--- javascript.js
+++ javascript.js
@@ -1,9 +1,18 @@
+const operatorMap = {
+    "+": "add",
+    "-": "subtract",
+    "*": "multiply",
+    "/": "divide"
+};
+
 const calculator = {
     add: (a, b) => a + b,
     subtract: (a, b) => a - b,
     multiply: (a, b) => a * b,
     divide: (a, b) => a / b
 };
+
+const operate = (operator, firstNum, secondNum) => calculator[operatorMap[operator]](firstNum, secondNum);
 
 
 
