136f717cee1ae2f9fd6a0947d1a52ef8d0abeb7e
Conflictive <callumtolland@gmail.com>
2025-12-06 18:05:58 +0000

    Implement calculator buttons updating display text on click

--- javascript.js
+++ javascript.js
@@ -1,3 +1,12 @@
+// ---VARIABLES---
+let equation = 0;
+
+// ---DOM ELEMENTS---
+const calcBtns = document.querySelectorAll(".calc-button");
+const display = document.querySelector("#display");
+
+
+// ---LOGIC---
 const operatorMap = {
     "+": "add",
     "-": "subtract",
@@ -13,6 +22,14 @@ const calculator = {
 };
 
 const operate = (operator, firstNum, secondNum) => calculator[operatorMap[operator]](firstNum, secondNum);
+
+// ---EVENT LISTENERS---
+calcBtns.forEach((button) => {
+    button.addEventListener("click", () => {
+        equation += button.textContent;
+        display.textContent = equation;
+    });
+})
 
 
 
